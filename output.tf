d3fb35ccad54040cd7fa979c94a98c70ab56ab23
Aaron Wells <wellsaar@iu.edu>
2022-01-28 16:00:00 -0500

    setup follower and leader sections

--- output.tf
+++ output.tf
@@ -3,7 +3,12 @@
 ################
 
 
-output "floating_ip_ubuntu20" {
-  value = openstack_networking_floatingip_v2.terraform_floatip_ubuntu20.*.address
-  description = "Public IP for Ubuntu 20"
+output "floating_ip_ubuntu20_leader" {
+  value = openstack_networking_floatingip_v2.terraform_floatip_ubuntu20_leader.*.address
+  description = "Public IP for Ubuntu 20 leader"
+}
+
+output "floating_ip_ubuntu20_follower" {
+  value = openstack_networking_floatingip_v2.terraform_floatip_ubuntu20_follower.*.address
+  description = "Public IP for Ubuntu 20 followers"
 }
