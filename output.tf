58cf0f6335f2bcb81541083260d7c482cfc7da20
Aaron Wells <wellsaar@iu.edu>
2022-01-31 15:43:03 -0500

    setup terraform template that will generate ansible inventory

--- output.tf
+++ output.tf
@@ -12,3 +12,13 @@ output "floating_ip_ubuntu20_follower" {
   value = openstack_networking_floatingip_v2.terraform_floatip_ubuntu20_follower.*.address
   description = "Public IP for Ubuntu 20 followers"
 }
+
+resource "local_file" "ansible_inventory" {
+  content = templatefile("${path.module}/inventory.tftpl",
+  {
+    ansible_leader_ip    = openstack_networking_floatingip_v2.terraform_floatip_ubuntu20_leader.*.address,
+    ansible_follower_ip  = openstack_networking_floatingip_v2.terraform_floatip_ubuntu20_follower.*.address,
+  }
+  )
+  filename  = "ansible/inventory.ini"
+}
